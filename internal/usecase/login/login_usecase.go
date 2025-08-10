6fc64d4b533ac77cdba3ec2e52ee532b3f142ab1
Hayato Mitomi <mitomihayato@mitomihayatonoMacBook-Pro.local>
2025-08-10 10:23:21 +0900

    feat: implement login usecase

---
+++ internal/usecase/login/login_usecase.go
@@ -0,0 +1,48 @@
+package login
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/Haya372/go-samples/internal/domain/service"
+	"github.com/Haya372/go-samples/internal/domain/user"
+	"github.com/Haya372/go-samples/internal/domain/vo"
+
+	customErr "github.com/Haya372/go-samples/internal/error"
+)
+
+type LoginParam struct {
+	UserId      vo.UserId
+	RawPassword string
+}
+
+type LoginUsecase interface {
+	Execute(ctx context.Context, param LoginParam) (*user.User, error)
+}
+
+type loginUsecaseImpl struct {
+	userRepository          user.UserRepository
+	userAuthenticateService service.UserAuthenticateService
+}
+
+func (uc *loginUsecaseImpl) Execute(ctx context.Context, param LoginParam) (*user.User, error) {
+	user, err := uc.userRepository.FindByEmail(ctx, param.UserId)
+	if err != nil {
+		return nil, err
+	} else if user == nil {
+		return nil, customErr.NewAuthenticationError(fmt.Errorf("user not found, userId=%v", param.UserId))
+	}
+
+	if ok := uc.userAuthenticateService.Execute(ctx, *user, param.RawPassword); !ok {
+		return nil, customErr.NewAuthenticationError(fmt.Errorf("invalid password, userId=%v", param.UserId))
+	}
+
+	return user, nil
+}
+
+func NewLoginUsecase(userRepository user.UserRepository, userAuthenticateService service.UserAuthenticateService) LoginUsecase {
+	return &loginUsecaseImpl{
+		userRepository:          userRepository,
+		userAuthenticateService: userAuthenticateService,
+	}
+}
